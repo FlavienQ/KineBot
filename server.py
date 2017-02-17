ecff5f5d73d39ff774a01d60826c86a11f9a2ed7
KineBotTeam <flquijoux@gmail.com>
2017-02-17 18:15:33 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template
-from flask.ext.socketio import SocketIO
+from flask.ext.socketio import SocketIO, emit, send
 import json
 
 
@@ -11,13 +11,18 @@ socketio = SocketIO(app)
 @app.route("/")
 def index():
     return render_template('index.html')
+	
+@socketio.on('send_message')
+def handle_source(json_data):
+    text = json_data['message'].encode('ascii', 'ignore')
+    socketio.emit('echo', {'echo': 'Server Says: '+text})
 
-if __name__ = '__main__':
-	from werkzeug.wsgi import SharedDataMiddleware
-	app = SharedDataMiddleware(app, {
-        '/': os.path.join(os.path.dirname(__file__), 'public')
-        })
+if __name__ =='__main__':
+	#from werkzeug.wsgi import SharedDataMiddleware
+	#app = SharedDataMiddleware(app, {
+    #    '/': os.path.join(os.path.dirname(__file__), 'public')
+    #    })
 	port = int(os.environ.get('PORT', 5000))
 	print('Listening on local port...')
 	app.debug = True
-	app.run(host='0.0.0.0', port=port)
+	socketio.run(app, host='0.0.0.0', port=port)
